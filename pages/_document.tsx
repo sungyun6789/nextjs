fc9d6249efa240cd0b94bb992ac88ffde17c5d8a
sungyun <sungyun5423@gmail.com>
2021-08-17 21:29:24 +0900

    [Next-TODO] global style

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,16 +1,15 @@
-import Document from "next/document";
-import { ServerStyleSheet } from "styled-components";
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx: any) {
+  static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
 
     try {
       ctx.renderPage = () =>
         originalRenderPage({
-          enhanceApp: (App: any) => (props: any) =>
-            sheet.collectStyles(<App {...props} />),
+          enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
         });
 
       const initialProps = await Document.getInitialProps(ctx);
@@ -26,5 +25,20 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal();
     }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link href="https://fonts.googleapis.com/css?family=Noto+Sans:400,700&display=swap" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css?family=Noto+Sans+KR:400,700&display=swap" rel="stylesheet" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,23 @@
-import { NextPage } from "next";
-import styled from "styled-components";
+import { NextPage } from 'next';
+import styled from 'styled-components';
 
 const Container = styled.div`
-  font-style: italic;
+  padding: 20px;
 `;
 
 const index: NextPage = () => {
-  return <Container>hello styled-components!</Container>;
+  return (
+    <Container>
+      <h1>hello styled-components</h1>
+      <h2>hello styled-components</h2>
+      <p>hello styled-components</p>
+      <ul>
+        <li>hello styled-components</li>
+      </ul>
+      <a>hello styled-components</a>
+      <span>hello styled-components</span>
+    </Container>
+  );
 };
 
 export default index;
