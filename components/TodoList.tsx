d9d16b0bedf53e3d0f08aa204487a265cd3f8b51
sungyun <sungyun5423@gmail.com>
2021-08-19 00:57:42 +0900

    [NextTodo] add svg component

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -1,7 +1,10 @@
-import React, { useCallback, useMemo } from 'react';
+import React, { useMemo } from 'react';
 import styled from 'styled-components';
 import palette from '../styles/palette';
 import { TodoType } from '../types/todo';
+
+import TrashCanIcon from '../public/static/svg/trash_can.svg';
+import CheckMarkIcon from '../public/static/svg/check_mark.svg';
 
 const Container = styled.div`
   width: 100%;
@@ -90,8 +93,22 @@ const Container = styled.div`
 
       .todo-right-side {
         display: flex;
+        align-items: center;
         margin-right: 12px;
-
+        svg {
+          &:first-child {
+            margin-right: 16px;
+          }
+        }
+        .todo-trash-can {
+          width: 16px;
+          path {
+            fill: ${palette.deep_red};
+          }
+        }
+        .todo-check-mark {
+          fill: ${palette.deep_green};
+        }
         .todo-button {
           width: 20px;
           height: 20px;
@@ -152,6 +169,12 @@ const TodoList: React.FC<IProps> = ({ todos }) => {
               <p className={`todo-text ${todo.checked ? 'checked-todo-text' : ''}`}>{todo.text}</p>
             </div>
             <div className="todo-right-side">
+              {todo.checked && (
+                <>
+                  <TrashCanIcon className="todo-trash-can" onClick={() => {}} />
+                  <CheckMarkIcon className="todo-check-mark" onClick={() => {}} />
+                </>
+              )}
               {!todo.checked && <button type="button" className="todo-button" onClick={() => {}} />}
             </div>
           </li>
