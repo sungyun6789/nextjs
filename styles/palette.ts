27c8f1221874a51fa98829ea54fdc5d9ab16b182
sungyun <sungyun5423@gmail.com>
2021-08-19 00:05:34 +0900

    [NextTodo] create TodoList & TodoItem

--- styles/palette.ts
+++ styles/palette.ts
@@ -4,7 +4,7 @@ export default {
   yellow: '#FCFFB0',
   green: '#E2FFAF',
   blue: '#AEE4FF',
-  navy: 'B5C7ED',
+  navy: '#B5C7ED',
   gray: '#E5E5E5',
   deep_red: '#F35456',
   deep_green: '#47E774',
