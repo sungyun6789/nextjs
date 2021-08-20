740fd59934e0612b8b76531abda5656117d22c9c
sungyun <sungyun5423@gmail.com>
2021-08-21 01:40:35 +0900

    [NextTodo] add Todo

---
+++ pages/todo/add.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from 'next';
+import React from 'react';
+import AddTodo from '../../components/AddTodo';
+
+const todo: NextPage = () => {
+  return <AddTodo />;
+};
+
+export default todo;
