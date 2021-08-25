f4c828487b7b16e4c1f2e32df49dfa0f5e0c9df7
sungyun <sungyun5423@gmail.com>
2021-08-26 00:13:39 +0900

    [NextBnb] Input Component apply in SignUpModal Component

--- NextBnb/components/auths/SignUpModal.tsx
+++ NextBnb/components/auths/SignUpModal.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import CloseXIcon from '../../public/static/svg/modal/modal_colose_x_icon.svg';
 import MailIcon from '../../public/static/svg/auth/mail.svg';
@@ -6,6 +6,7 @@ import PersonIcon from '../../public/static/svg/auth/person.svg';
 import OpendEyeIcon from '../../public/static/svg/auth/opened_eye.svg';
 import ClosedEyeIcon from '../../public/static/svg/auth/closed_eye.svg';
 import palette from '../../styles/palette';
+import Input from '../common/Input';
 
 const Container = styled.div`
   width: 568px;
@@ -41,27 +42,79 @@ const Container = styled.div`
       top: 16px;
     }
   }
+  .sign-up-password-input-wrapper {
+    svg {
+      cursor: pointer;
+    }
+  }
 `;
 
 const SignUpModal: React.FC = () => {
+  const [email, setEmail] = useState('');
+  const [lastname, setLastname] = useState('');
+  const [firstname, setFirstname] = useState('');
+  const [password, setPassword] = useState('');
+
+  // 이메일 주소 변경 시
+  const onChangeEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+
+  // 이름 주소 변경 시
+  const onChangeLastname = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setLastname(event.target.value);
+  };
+
+  // 성 변경 시
+  const onChangeFirstname = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFirstname(event.target.value);
+  };
+
+  // 비밀번호 변경 시
+  const onChangePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  const [hidePassword, setHidePassword] = useState(true);
+
+  // 비밀번호 숨김 토글하기
+  const toggleHidePassword = () => {
+    setHidePassword(!hidePassword);
+  };
+
   return (
     <Container>
       <CloseXIcon className="modal-close-x-icon" />
       <div className="input-wrapper">
-        <input placeholder="이메일 주소" type="email" name="email" />
-        <MailIcon />
+        <Input
+          placeholder="이메일 주소"
+          type="email"
+          icon={<MailIcon />}
+          name="email"
+          value={email}
+          onChange={onChangeEmail}
+        />
       </div>
       <div className="input-wrapper">
-        <input placeholder="이름(예:길동)" />
-        <PersonIcon />
+        <Input placeholder="이름(예:길동)" icon={<PersonIcon />} value={lastname} onChange={onChangeLastname} />
       </div>
       <div className="input-wrapper">
-        <input placeholder="성(예:홍)" />
-        <PersonIcon />
+        <Input placeholder="성(예:홍)" icon={<PersonIcon />} value={firstname} onChange={onChangeFirstname} />
       </div>
-      <div className="input-wrapper">
-        <input placeholder="비밀번호 설정하기" type="password" />
-        <OpendEyeIcon />
+      <div className="input-wrapper sign-up-password-input-wrapper">
+        <Input
+          placeholder="비밀번호 설정하기"
+          type={hidePassword ? 'password' : 'text'}
+          icon={
+            hidePassword ? (
+              <ClosedEyeIcon onClick={toggleHidePassword} />
+            ) : (
+              <OpendEyeIcon onClick={toggleHidePassword} />
+            )
+          }
+          value={password}
+          onChange={onChangePassword}
+        />
       </div>
     </Container>
   );
