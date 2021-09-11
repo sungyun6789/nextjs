afb3e67c2ec9a5f0f8f9ceb3581ed75b5c34d347
sungyun <sungyun5423@gmail.com>
2021-09-12 00:03:19 +0900

    [NextBnb] create header component user-menu

--- NextBnb/components/Header.tsx
+++ NextBnb/components/Header.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import AirbnbLogoIcon from '../public/static/svg/logo/logo.svg';
 import AirbnbLogoTextIcon from '../public/static/svg/logo/logo_text.svg';
@@ -10,6 +10,7 @@ import HamburgerIcon from '../public/static/svg/header/hamburger.svg';
 import { useDispatch } from 'react-redux';
 import { authActions } from '../store/auth';
 import AuthModal from './auths/AuthModal';
+import OutsideClickHandler from 'react-outside-click-handler';
 
 const Container = styled.div`
   position: sticky;
@@ -23,11 +24,35 @@ const Container = styled.div`
   background-color: white;
   box-shadow: rgba(0, 0, 0, 0.08) 0px 1px 12px;
   z-index: 10;
-  .header-logo-wrapper {
-    display: flex;
-    align-items: center;
-    .header-logo {
-      margin-right: 6px;
+  .header-logo-wrapper + div {
+    position: relative;
+  }
+
+  .header-usermenu {
+    position: absolute;
+    right: 0;
+    top: 52px;
+    width: 240px;
+    padding: 8px 0;
+    box-shadow: 0 2px 16px rgba(0, 0, 0, 0.12);
+    border-radius: 8px;
+    background-color: white;
+    li {
+      display: flex;
+      align-items: center;
+      width: 100%;
+      height: 42px;
+      padding: 0 16px;
+      cursor: pointer;
+      &:hover {
+        background-color: ${palette.gray_f7};
+      }
+    }
+    .header-usermenu-divider {
+      width: 100%;
+      height: 1px;
+      margin: 8px 0;
+      background-color: ${palette.gray_dd};
     }
   }
 
@@ -91,6 +116,9 @@ const Header: React.FC = () => {
   const user = useSelector((state) => state.user);
   const dispatch = useDispatch();
 
+  // 유저 메뉴 열고, 닫힘 메뉴
+  const [isUsermenuOpend, setIsUsermenuOpend] = useState(false);
+
   return (
     <Container>
       <Link href="/">
@@ -124,10 +152,36 @@ const Header: React.FC = () => {
         </div>
       )}
       {user.isLogged && (
-        <button className="header-user-profile" type="button">
-          <HamburgerIcon />
-          <img src={user.profileImage} className="header-user-profile-image" alt="" />
-        </button>
+        <OutsideClickHandler
+          onOutsideClick={() => {
+            if (isUsermenuOpend) {
+              setIsUsermenuOpend(false);
+            }
+          }}
+        >
+          <button
+            className="header-user-profile"
+            type="button"
+            onClick={() => setIsUsermenuOpend(!isUsermenuOpend)}
+          >
+            <HamburgerIcon />
+            <img src={user.profileImage} className="header-user-profile-image" alt="" />
+          </button>
+          {isUsermenuOpend && (
+            <ul className="header-usermenu">
+              <li>숙소 관리</li>
+              <Link href="/room/register/building">
+                <a role="presentation" onClick={() => setIsUsermenuOpend(false)}>
+                  <li>숙소 등록하기</li>
+                </a>
+              </Link>
+              <div className="header-usermenu-divider" />
+              <li role="presentation" onClick={() => {}}>
+                로그아웃
+              </li>
+            </ul>
+          )}
+        </OutsideClickHandler>
       )}
       <ModalPortal>
         <AuthModal closeModal={closeModal} />
