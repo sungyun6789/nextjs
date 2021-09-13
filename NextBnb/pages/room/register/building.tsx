f25b39faa474a10007b74a4db1b77e9ca2697352
sungyun <sungyun5423@gmail.com>
2021-09-14 00:14:30 +0900

    [NextBnb] create RegisterRoomBuilding & building

---
+++ NextBnb/pages/room/register/building.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from 'next';
+import React from 'react';
+import RegisterRoomBuilding from '../../../components/register/RegisterRoomBuilding';
+
+const building: NextPage = () => {
+  return <RegisterRoomBuilding />;
+};
+
+export default building;
