0ec4e361ff10023938df5da73a822d06ea6859c8
sungyun <sungyun5423@gmail.com>
2021-09-18 01:02:13 +0900

    [NextBnb] added BedRoom Count & Bed Count

--- NextBnb/components/room/register/RegisterRoomBedrooms.tsx
+++ NextBnb/components/room/register/RegisterRoomBedrooms.tsx
@@ -5,6 +5,9 @@ import palette from '../../../styles/palette';
 import { useDispatch } from 'react-redux';
 import { registerRoomActions } from '../../../store/registerRoom';
 import Counter from '../../common/Counter';
+import { getNumber } from '../../../lib/utils';
+import Selector from '../../common/Selector';
+import { bedroomCountList } from '../../../lib/staticData';
 
 const Container = styled.div`
   padding: 62px 30px 100px;
@@ -30,16 +33,33 @@ const Container = styled.div`
     margin-top: 24px;
     margin-bottom: 32px;
   }
+  .register-room-bedroom-count-wrapper {
+    width: 320px;
+    margin-bottom: 32px;
+  }
+  .register-room-bed-count-wrapper {
+    width: 320px;
+    margin-bottom: 57px;
+  }
 `;
 
 const RegisterRoomBedrooms: React.FC = () => {
-  const maximumGuestCount = useSelector((state) => state.registerRoom.maximumGuestCount);
   const dispatch = useDispatch();
+  const maximumGuestCount = useSelector((state) => state.registerRoom.maximumGuestCount);
+  const bedroomCount = useSelector((state) => state.registerRoom.bedroomCount);
+  const bedCount = useSelector((state) => state.registerRoom.bedCount);
 
   // 최대 숙박 인원 변경 시
   const onChangeMaximumGuestCount = (value: number) => {
     dispatch(registerRoomActions.setMaximumGuestCount(value));
   };
+
+  // 침실 개수 변경 시
+  const onChangeBedroomCount = (event: React.ChangeEvent<HTMLSelectElement>) =>
+    dispatch(registerRoomActions.setBedroomCount(getNumber(event.target.value) || 0));
+
+  // 침대 개수 변경 시
+  const onChangeBedCount = (value: number) => dispatch(registerRoomActions.setBedCount(value));
 
   return (
     <Container>
@@ -54,6 +74,18 @@ const RegisterRoomBedrooms: React.FC = () => {
           value={maximumGuestCount}
           onChange={onChangeMaximumGuestCount}
         />
+      </div>
+      <div className="register-room-bedroom-count-wrapper">
+        <Selector
+          type="register"
+          value={`침실 ${bedroomCount}개`}
+          onChange={onChangeBedroomCount}
+          label="게스트가 사용할 수 있는 침실은 몇 개인가요?"
+          options={bedroomCountList}
+        />
+      </div>
+      <div className="register-room-bed-count-wrapper">
+        <Counter label="침대" value={bedCount} onChange={onChangeBedCount} />
       </div>
     </Container>
   );
--- NextBnb/lib/utils.ts
+++ NextBnb/lib/utils.ts
@@ -12,3 +12,14 @@ export const cookieStringToObject = (cookieString: string | undefined) => {
   }
   return cookies;
 };
+
+// string에서 number만 return하는 함수
+export const getNumber = (string: string) => {
+  // match - 문자열이 정규식과 매치되는 부분을 검색
+  // join - 배열의 모든 요소를 연결해 하나의 문자열로 만듬
+  const numbers = string.match(/\d/g)?.join('');
+  if (numbers) {
+    return Number(numbers);
+  }
+  return null;
+};
