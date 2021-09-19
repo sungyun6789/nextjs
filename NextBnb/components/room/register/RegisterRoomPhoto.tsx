9c7e3de2cebc50a46ec4ea8fe0e6454138ac8e4e
sungyun <sungyun5423@gmail.com>
2021-09-20 02:08:19 +0900

    [NextBnb] added File-Upload Button

--- NextBnb/components/room/register/RegisterRoomPhoto.tsx
+++ NextBnb/components/room/register/RegisterRoomPhoto.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
 import palette from '../../../styles/palette';
+import isEmpty from 'lodash/isEmpty';
+import { useSelector } from '../../../store';
+import UploadIcon from '../../../public/static/svg/register/upload.svg';
+import Button from '../../common/Button';
 
 const Container = styled.div`
   padding: 62px 30px 100px;
@@ -19,9 +23,40 @@ const Container = styled.div`
     max-width: 400px;
     margin-bottom: 24px;
   }
+  .register-room-upload-photo-wrapper {
+    width: 858px;
+    height: 433px;
+    margin: auto;
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: 2px dashed ${palette.gray_bb};
+    border-radius: 6px;
+
+    input {
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      opacity: 0;
+      cursor: pointer;
+    }
+    img {
+      width: 100%;
+      max-height: 100%;
+    }
+  }
 `;
 
 const RegisterRoomPhoto: React.FC = () => {
+  const photos = useSelector((state) => state.registerRoom.photos);
+
+  // 이미지 업로드 하기
+  const uploadImage = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { files } = event.target;
+    console.log(files);
+  };
+
   return (
     <Container>
       <h2>숙소 사진 올리기</h2>
@@ -30,6 +65,16 @@ const RegisterRoomPhoto: React.FC = () => {
         게스트가 사진을 보고 숙소의 느낌을 생생히 떠올려볼 수 있도록 해주세요. 우선 사진 1장을
         업로드하고 숙소를 등록한 후에 추가할 수 있습니다.
       </p>
+      {isEmpty(photos) && (
+        <div className="register-room-upload-photo-wrapper">
+          <>
+            <input type="file" accept="image/*" onChange={uploadImage} />
+            <Button icon={<UploadIcon />} color="bittersweet" width="167px">
+              사진 업로드
+            </Button>
+          </>
+        </div>
+      )}
     </Container>
   );
 };
