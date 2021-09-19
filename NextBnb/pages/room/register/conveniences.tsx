f38699c21676fc9f7aaaf1f12d5b817f1a51381f
sungyun <sungyun5423@gmail.com>
2021-09-20 01:52:11 +0900

    [NextBnb] create RegisterRoom 6step page

---
+++ NextBnb/pages/room/register/conveniences.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { NextPage } from 'next';
+import RegisterConveniences from '../../../components/room/register/RegisterConveniences';
+
+const conveniences: NextPage = () => {
+  return <RegisterConveniences />;
+};
+
+export default conveniences;
