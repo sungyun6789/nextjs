8fe86868adf3a9e76fbb3faf9915fdb0fa642bad
sungyun <sungyun5423@gmail.com>
2021-09-23 01:52:30 +0900

    [NextBnb] fix Textarea style & apply Textarea

--- NextBnb/components/common/Textarea.tsx
+++ NextBnb/components/common/Textarea.tsx
@@ -4,23 +4,21 @@ import styled from 'styled-components';
 import palette from '../../styles/palette';
 
 const StyledTextarea = styled(ReactAutosizeTextarea)`
-  textarea {
-    position: relative;
-    width: 100%;
-    min-height: 216px;
-    padding: 11px;
-    border: 1px solid ${palette.gray_eb};
-    border-radius: 4px;
-    font-size: 16px;
-    outline: none;
-    resize: none;
-    font: inherit;
-    &::placeholder {
-      color: ${palette.gray_76};
-    }
-    &:focus {
-      border-color: ${palette.dark_cyan};
-    }
+  position: relative;
+  width: 100%;
+  min-height: 216px;
+  padding: 11px;
+  border: 1px solid ${palette.gray_eb};
+  border-radius: 4px;
+  font-size: 16px;
+  outline: none;
+  resize: none;
+  font: inherit;
+  & ::placeholder {
+    color: ${palette.gray_76};
+  }
+  & :focus {
+    border-color: ${palette.dark_cyan};
   }
 `;
 
