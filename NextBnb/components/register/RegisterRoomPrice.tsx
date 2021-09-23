a0031c1203159330618eecf96b53492fc37018fe
sungyun <sungyun5423@gmail.com>
2021-09-23 21:30:25 +0900

    [NextBnb] create makeMoneyString utils & onChangePrice

--- NextBnb/components/register/RegisterRoomPrice.tsx
+++ NextBnb/components/register/RegisterRoomPrice.tsx
@@ -5,6 +5,8 @@ import RegisterRoomFooter from './RegisterRoomFooter';
 import palette from '../../styles/palette';
 import { useSelector } from '../../store';
 import Input from '../common/Input';
+import { registerRoomActions } from '../../store/registerRoom';
+import { makeMoneyString } from '../../lib/utils';
 
 const Container = styled.div`
   padding: 62px 30px 100px;
@@ -26,11 +28,26 @@ const RegisterRoomPrice: React.FC = () => {
 
   const price = useSelector((state) => state.registerRoom.price);
 
+  // 금액 변경시
+  const onChangePrice = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const input = event.target.value;
+
+    const numberPrice = Number(input.replace(/,/g, ''));
+    // 인풋 값이 비워지면 price를 0으로 변경
+    if (numberPrice || numberPrice === 0) {
+      dispatch(registerRoomActions.setPrice(0));
+    }
+
+    if (numberPrice !== 0) {
+      dispatch(registerRoomActions.setPrice(numberPrice));
+    }
+  };
+
   return (
     <Container>
       <h2>숙소 요금 설정하기</h2>
       <h3>10단계</h3>
-      <Input label="기본요금" value={String(price)} />
+      <Input label="기본요금" value={makeMoneyString(String(price))} onChange={onChangePrice} />
       <RegisterRoomFooter prevHref="/room/register/title" nextHref="/room/register/date" />
     </Container>
   );
