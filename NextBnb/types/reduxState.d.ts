aeba00df7f86db81aaed63868fbd059ee5686548
sungyun <sungyun5423@gmail.com>
2021-09-25 18:36:07 +0900

    [NextBnb] create register room api

--- NextBnb/types/reduxState.d.ts
+++ NextBnb/types/reduxState.d.ts
@@ -9,3 +9,33 @@ export type UserState = UserType & {
 export type CommonState = {
   validateMode: boolean;
 };
+
+type RegisterRoomState = {
+  largeBuildingType: string | null;
+  buildingType: string | null;
+  roomType: string | null;
+  isSetUpForGuest: boolean | null;
+  maximumGuestCount: number;
+  bedroomCount: number;
+  bedCount: number;
+  bedList: { id: number; beds: { type: BedType; count: number }[] }[];
+  publicBedList: { type: BedType; count: number }[];
+  bathroomCount: number;
+  bathroomType: 'private' | 'public' | null;
+  country: string;
+  city: string;
+  district: string;
+  streetAddress: string;
+  detailAddress: string;
+  postcode: string;
+  latitude: number;
+  longitude: number;
+  amentities: string[];
+  conveniences: string[];
+  photos: string[];
+  description: string;
+  title: string;
+  price: number;
+  startDate: string | null;
+  endDate: string | null;
+};
