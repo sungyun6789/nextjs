f1a36701b493d4e92e5dee4644f268b1981c7a52
sungyun <sungyun5423@gmail.com>
2021-09-25 23:43:39 +0900

    [NextBnb] SearchRoom redux setting

--- NextBnb/store/index.ts
+++ NextBnb/store/index.ts
@@ -5,12 +5,14 @@ import user from './user';
 import common from './common';
 import auth from './auth';
 import registerRoom from './registerRoom';
+import searchRoom from './searchRoom';
 
 const rootReducer = combineReducers({
   common: common.reducer,
   user: user.reducer,
   auth: auth.reducer,
   registerRoom: registerRoom.reducer,
+  searchRoom: searchRoom.reducer,
 });
 
 // 스토어 타입
