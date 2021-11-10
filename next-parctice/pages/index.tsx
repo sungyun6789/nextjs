e9be1a69de2d08c0053a05da5a2980b80589be69
sungyun <sungyun5423@gmail.com>
2021-11-11 00:13:48 +0900

    SSR, SSG 차이점 익히기

--- next-parctice/pages/index.tsx
+++ next-parctice/pages/index.tsx
@@ -22,23 +22,9 @@ const Home: NextPage = ({ posts }: any) => {
 
 export default Home;
 
-export const getServerSideProps = async () => {
-  const res = await fetch(
-    `http://jsonplaceholder.typicode.com/posts?_start=0&_end=10`
-  );
-  const posts = await res.json();
-
-  return {
-    props: {
-      posts,
-    },
-  };
-};
-
-// export const getStaticProps = async () => {
-//   const res = await fetch(
-//     `http://jsonplaceholder.typicode.com/posts?_start=0&_end=10`
-//   );
+// 서버에서 데이터가 변경되면 같이 업데이트 됨
+// export const getServerSideProps = async () => {
+//   const res = await fetch(`http://localhost:8080/api/posts`);
 //   const posts = await res.json();
 
 //   return {
@@ -47,3 +33,21 @@ export const getServerSideProps = async () => {
 //     },
 //   };
 // };
+
+/* 
+처음 시작할 때 데이터를 가져오기 업데이트 되지 않음
+페이지가 즉각적으로 업데이트 되지 않는 상황에서 사용
+빌드할 때 페이지 업데이트
+*/
+export const getStaticProps = async () => {
+  const res = await fetch(`http://localhost:8080/api/posts`);
+  const posts = await res.json();
+
+  return {
+    props: {
+      posts,
+    },
+    // 접속하고 20초가 지나면 데이터를 다시 받아옴
+    revalidate: 20,
+  };
+};
