bb9733a48b4bbb1b55135b8e27ea4176287103de
sungyun <sungyun5423@gmail.com>
2021-11-10 23:47:33 +0900

    노드 서버를 사용하기 위해 임시 저장

--- next-parctice/pages/index.tsx
+++ next-parctice/pages/index.tsx
@@ -1,7 +1,8 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 
-const Home: NextPage = () => {
+const Home: NextPage = ({ posts }: any) => {
+  console.log(posts);
   return (
     <div>
       <Head>
@@ -10,8 +11,39 @@ const Home: NextPage = () => {
         <meta content="My Blog Contents" />
       </Head>
       <h1>Welcome to My Blog</h1>
+      <ul>
+        {posts.map((post: any) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
     </div>
   );
 };
 
 export default Home;
+
+export const getServerSideProps = async () => {
+  const res = await fetch(
+    `http://jsonplaceholder.typicode.com/posts?_start=0&_end=10`
+  );
+  const posts = await res.json();
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
+
+// export const getStaticProps = async () => {
+//   const res = await fetch(
+//     `http://jsonplaceholder.typicode.com/posts?_start=0&_end=10`
+//   );
+//   const posts = await res.json();
+
+//   return {
+//     props: {
+//       posts,
+//     },
+//   };
+// };
